0ac6fd18ec0ea3a3ae942a86b02e57e13ed19367
k_pulik <63879539+kbruh@users.noreply.github.com>
2024-03-13 12:46:59 -0700

    put everything back into one file for convenience

--- linear_algebra_calc.py
+++ linear_algebra_calc.py
@@ -1,5 +1,4 @@
 import numpy as np
-from choices_class import Choices as choices
 
 # Get matrix dimensions from user
 rows = int(input("Enter number of rows (m): "))
@@ -17,4 +16,99 @@ for i in range(rows):
 print("Original matrix:")
 print(matrix)
 
-choices(matrix)
+class LinAlFuncs:
+
+    def __init__(self, matrix, rows, cols):
+        self.matrix = matrix
+        self.rows = rows
+        self.cols = cols
+        
+    # Identity Matrix
+    def identity(self):
+        id = np.identity(self.rows)
+        print("Identity matrix:")
+        print(id)
+
+    # Find rank
+    def rank(self):
+        print("Rank of matrix:")
+
+    # Find determinant
+    def determinant(self):
+        print("Determinant of matrix:")
+
+    # Find inverse
+    def inverse(self):
+        print("Inverse of matrix:")
+
+    # Find eigenvalues
+    def eigenvalues(self):
+        print("Eigenvalues of matrix:")
+
+    # Find eigenvectors
+    def eigenvectors(self):
+        print("Eigenvectors of matrix:")
+
+    # Find nullity
+    def nullity(self):
+        print("Nullity of matrix:")
+
+    # Find dimension
+    def dimension(self):
+        print("Dimension of matrix:")
+
+    # Find multiplicity
+    def multiplicity(self):
+        print("Multiplicity of matrix:")
+
+    # Find linear (in)depenance
+    def linear_independence(self):
+        print("Linear indepenance of matrix:")
+
+    # Find transpose
+    def transpose(self):
+        print("Transpose of matrix:")
+        tp = np.transpose(self.matrix)
+        print(tp)
+        
+
+
+f = LinAlFuncs(matrix, rows, columns)
+
+choice = input("What would you like to do with the matrix? (Type 'rank', 'det', 'inv', 'eigVal', 'eigVec', 'null', 'dim','mult', 'lindep', 'transp', 'all', or 'exit'): ")
+
+if choice == "rank":
+    f.rank()
+elif choice == "det":
+    f.determinant()  
+elif choice == "inv":
+    f.inverse()
+elif choice == "eigVal":
+    f.eigenvalues()
+elif choice == "eigVec":
+    f.eigenvectors()
+elif choice == "null":
+    f.nullity()
+elif choice == "dim":
+    f.dimension()
+elif choice == "mult":
+    f.multiplicity()
+elif choice == "lindep":
+    f.linear_independence()
+elif choice == "transp":
+    f.transpose()
+elif choice == "all":
+    f.rank()
+    f.determinant()
+    f.inverse()
+    f.eigenvalues()
+    f.eigenvectors()
+    f.nullity()
+    f.dimension()
+    f.multiplicity()
+    f.linear_independence()
+    f.transpose()
+elif choice == "exit":
+    print("Exiting program.")
+else:
+    print("Invalid choice.")
